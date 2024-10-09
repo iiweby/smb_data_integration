1e858f82c3e912cc667764588b52424112ee22b2
iiweby <danielparedesmilian@gmail.com>
2024-10-09 17:11:36 -0500

    Added Color Pallete

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,13 @@
 export const content = ["./src/**/*.{js,jsx,ts,tsx}"];
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      primary: "#0076A8",
+      secondary: "#00A4E4",
+      background: "#F4F4F4",
+      accent: "#E6D66C",
+      highlight: "#FFA300",
+    },
+  },
 };
 export const plugins = [];
