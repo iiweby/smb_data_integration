af9303eb56eab8ff162dac1acf028aa6b00c7e16
Akanksha192001 <akankshaneelapala@gmail.com>
2024-10-16 15:41:31 -0500

    added header and footer
    We modified App.js and we added Footer.js and Header.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
 import SubmitRequest from "./components/SubmitRequest";
 import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<SubmitRequest />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
+      <div className="bg-background min-h-screen flex flex-col justify-between">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<SubmitRequest />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
